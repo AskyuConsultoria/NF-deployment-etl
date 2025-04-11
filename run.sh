e462726de13773ac1e4f3aacfe2059c16023815d
kayky <kayky.pedroso@sptech.school>
2025-04-11 18:51:08 -0300

    Feat(Bash): Criação de scripts para automatização do processo

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ODATE=$1
+
+if [ -z "$ODATE"]; then
+    ODATE=$(TZ=America/Sao_Paulo date +%Y%m%d)
+fi
+
+sudo docker run --rm \
+  -v ~/raw/NFS:/app/raw \
+  -v ~/trusted/NFS:/app/trusted \
+  --env-file .env \
+ python-image \
+  --odate $ODATE 
+
---
+++ scheduler.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+HORA=$1
+MINUTO=$2
+
+if [ -z "$HORA"]; then
+    HORA=17
+fi
+
+if [ -z "$MINUTO"]; then
+    HORA=0
+fi
+
+SCRIPT="/home/ubuntu/NF-deployment-etl/run.sh"
+
+LOG="/home/ubuntu/NF-deployment-etl/run.log"
+
+CRON_LINHA="$MINUTO $HORA * * * $SCRIPT >> $LOG 2>&1"
+
+# Verifica se já existe essa linha no crontab
+(crontab -l 2>/dev/null | grep -F "$SCRIPT") >/dev/null
+
+if [ $? -eq 0 ]; then
+    echo "A tarefa já está agendada no crontab."
+else
+    (crontab -l 2>/dev/null; echo "$CRON_LINHA") | crontab -
+    echo "Tarefa adicionada ao crontab com sucesso!"
+fi
